b7546f32f1968096034c90f6d0f65b9af438047e
Avi Gupta <guptaav@HCL.COM>
2018-07-25 23:12:33 +0530

    SeleniumDemoBackUp

---
+++ src/main/java/JavaInterviewQuestions/ReverseString.java
@@ -0,0 +1,16 @@
+package JavaInterviewQuestions;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+
+	String TestAutomation="TestAutomation";
+	int sele=TestAutomation.length();
+	System.out.println(sele);
+	
+	
+		
+		
+	}
+}
